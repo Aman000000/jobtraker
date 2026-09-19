a6fb2d8eb53cd0a1192086e12830a09f4def7b5c
Aman000000 <ansariaman04765@gmail.com>
2026-09-19 23:34:11 +0530

    fix: inline nav links to remove broken utils/links import

--- client/src/components/NavLinks.js
+++ client/src/components/NavLinks.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { IoBarChartSharp } from 'react-icons/io5';
+import { MdQueryStats } from 'react-icons/md';
+import { FaWpforms } from 'react-icons/fa';
+import { ImProfile } from 'react-icons/im';
 
-import links from '../utils/links';
+const links = [
+	{
+		id: 1,
+		text: 'stats',
+		path: '/',
+		icon: <IoBarChartSharp />,
+	},
+	{
+		id: 2,
+		text: 'all jobs',
+		path: 'all-jobs',
+		icon: <MdQueryStats />,
+	},
+	{
+		id: 3,
+		text: 'add job',
+		path: 'add-job',
+		icon: <FaWpforms />,
+	},
+	{
+		id: 4,
+		text: 'profile',
+		path: 'profile',
+		icon: <ImProfile />,
+	},
+];
 
 const NavLinks = ({ toggleSidebar }) => {
 	return (
